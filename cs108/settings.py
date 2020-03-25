dd2679fca4e303a7e9f0438643b368db3aa5bc58
gsuissa <gsuissa@bu.edu>
2020-03-25 16:50:02 +0100

    New updates for Heroku deployment

--- cs108/settings.py
+++ cs108/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%urj)-uqdnmjs&@iv$fw3txkwdt3k3ajy9257^c*!alfpelwn1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
