e38d2bb1849caf73716251799a13d008fbfeb133
gsuissa <gsuissa@bu.edu>
2020-03-28 20:57:30 +0100

    commit

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -7,6 +7,6 @@ class ShowAllProfilesView(ListView):
     '''Create a subclass of ListView to display all quotes.'''
 
     model = Profile # retrieve objects of type Quote from the database
-    template_name = 'mini_fb/show_all_profiles.html'
+    template_name = 'templates/mini_fb/show_all_profiles.html'
     context_object_name = 'all_profiles' # how to find the data in the template file
 
