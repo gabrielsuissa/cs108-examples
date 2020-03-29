44c257d2edc427d6bfcc59f8bcb7842a75f171e2
gsuissa <gsuissa@bu.edu>
2020-03-29 12:58:08 +0200

    Added use of DetailView to display one random quote at a time

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,9 +2,11 @@
 # description: direct URL requests to view functions 
 
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, QuotePageView, RandomQuotePageview
 
 urlpatterns = [
     # map the URL (empty string) to the view
-    path('', HomePageView.as_view(), name='home'), # generic class based view
-]
+    path('', RandomQuotePageview.as_view(), name="random"), # pick one quote at random
+    path('all', HomePageView.as_view(), name='home'), # generic class based view
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), # Show one quote
+] 
