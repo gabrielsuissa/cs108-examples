16a759ed7cee29e3436ab6aeb9ee8b9369cf74f0
gsuissa <gsuissa@bu.edu>
2020-04-02 16:48:40 +0200

    Added use of DetailView to display one random quote at a time

--- quotes/urls.py
+++ quotes/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', RandomQuotePageview.as_view(), name="random"), # pick one quote at random
     path('all', HomePageView.as_view(), name='all'), # generic class based view
     path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), # Show one quote
-    path('quote/<int:pk>', UpdateQuoteView.as_view(), name='update_quote'), # Update a quote
+    path('quote/<int:pk>/update_quote', UpdateQuoteView.as_view(), name='update_quote'), # Update a quote
     path('person/<int:pk>', PersonPageView.as_view(), name='person'), # Show one person
     path('create_quote', CreateQuoteView.as_view(), name='create_quote'), # create a new quote with a form
 
